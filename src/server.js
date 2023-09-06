13e60741bce69a63de81ee4b90d1466d4d360781
Aman3612 <amandafribo@gmail.com>
2023-09-06 13:06:50 +0200

    Add files from zip

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const cors = require("cors")
+
 const app = express()
+//const customerRouter = require("..client/")
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello World")
